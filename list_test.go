6607b6b3aaffb951aaf07012da4617f3388b5edc
steden <1470804@qq.com>
2022-08-30 16:33:02 +0800

    单元测试覆盖率达到84.4%

--- list_test.go
+++ list_test.go
@@ -3,6 +3,7 @@ package collections
 import (
 	"fmt"
 	"github.com/stretchr/testify/assert"
+	"reflect"
 	"testing"
 )
 
@@ -26,4 +27,33 @@ func TestList_AsEnumerable(t *testing.T) {
 	lst.Add(4)
 
 	assert.Equal(t, 3, enumerable.Count())
+}
+
+func TestReflectItemType(t *testing.T) {
+	lst := NewList[int](1, 2, 3)
+	lstType := reflect.TypeOf(lst)
+	assert.Equal(t, reflect.TypeOf(1), ReflectItemType(lstType))
+	assert.NotEqual(t, reflect.TypeOf(true), ReflectItemType(lstType))
+}
+
+func TestReflectIsList(t *testing.T) {
+	lst := NewList[int](1, 2, 3)
+	type testStruct struct {
+	}
+	var test testStruct
+	assert.True(t, ReflectIsList(reflect.TypeOf(lst)))
+	assert.True(t, ReflectIsList(reflect.TypeOf(&lst)))
+	assert.False(t, ReflectIsList(reflect.TypeOf(1)))
+	assert.False(t, ReflectIsList(reflect.TypeOf(test)))
+}
+
+func TestReflectToArray(t *testing.T) {
+	lst := NewList[int](1, 2, 3)
+	lstValue := reflect.ValueOf(lst)
+	arr := ReflectToArray(lstValue)
+	assert.Equal(t, 3, len(arr))
+	assert.Equal(t, 1, arr[0])
+	assert.Equal(t, 2, arr[1])
+	assert.Equal(t, 3, arr[2])
+
 }
